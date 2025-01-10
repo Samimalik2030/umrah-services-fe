422ff61a88a1303e7411170397a6c4f4ba116959
Muhammad Sohaib Khan <skhan@turingnova.com>
2025-01-10 13:08:51 +0500

    chore: Interview scheduling binded

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,39 +1,116 @@
 import {
+  ActionIcon,
+  Badge,
+  Button,
   Card,
   Group,
   LoadingOverlay,
+  Menu,
+  Modal,
   Stack,
   Table,
-  Text,
   Title
 } from "@mantine/core";
+import { DateTimePicker } from "@mantine/dates";
 import useGetInterviews from "../../hooks/useGetInterviews";
-import { Interview } from "../../http/Api";
+import { Interview, UpdateInterviewDto } from "../../http/Api";
+import IconDots from "../../assets/icons/IconDots";
+import http from "../../http";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
 
 const Dashboard = () => {
   const { interviews, isLoading } = useGetInterviews();
+  const queryClient = useQueryClient();
+  const [opened, { open, close }] = useDisclosure(false);
+  const [selectedInterview, setSelectedInterview] = useState<Interview | null>(
+    null
+  );
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const { mutate: changeStatus, isPending: changingStatus } = useMutation({
+    mutationFn: ({ id, data }: { id: string; data: UpdateInterviewDto }) =>
+      http.admins.adminControllerPatchInterview(id, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["interviews"] });
+    }
+  });
+
+  const handleStatusChange = (id: string, data: UpdateInterviewDto) => {
+    changeStatus({ id, data });
+  };
+
+  const { mutate: scheduleInterview, isPending: schedulingInterview } =
+    useMutation({
+      mutationFn: ({ id, date }: { id: string; date: Date }) => {
+        const interviewDate = date.toISOString();
+        return http.admins.adminControllerPatchInterview(id, {
+          interviewDate: interviewDate,
+          interviewTime: interviewDate
+        });
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["interviews"] });
+        close();
+      },
+      onError: (error) => {
+        console.error("Error scheduling interview:", error);
+      }
+    });
+
+  const handleEditClick = (interview: Interview) => {
+    setSelectedInterview(interview);
+    setSelectedDate(new Date(interview.date));
+    open();
+  };
+
+  const handleUpdateClick = () => {
+    if (selectedInterview && selectedDate) {
+      scheduleInterview({ id: selectedInterview.id, date: selectedDate });
+    } else {
+      console.error("Invalid date or interview selection");
+    }
+  };
+
   return (
     <Stack p={"xl"}>
-      <LoadingOverlay visible={isLoading} />
+      <LoadingOverlay
+        visible={isLoading || changingStatus || schedulingInterview}
+      />
       <Title order={3}>Students Applications</Title>
       <Card>
         <Table>
           <Table.Thead>
             <Table.Tr>
-              <Table.Th>Name</Table.Th>
-              <Table.Th>Email</Table.Th>
-              <Table.Th>Status</Table.Th>
-              <Table.Th>Scheduled Date</Table.Th>
-              <Table.Th>Scheduled Time</Table.Th>
-              <Table.Th>Actions</Table.Th>
+              <Table.Th ta={"center"}>Name</Table.Th>
+              <Table.Th ta={"center"}>Email</Table.Th>
+              <Table.Th ta={"center"}>Status</Table.Th>
+              <Table.Th ta={"center"}>Scheduled Date</Table.Th>
+              <Table.Th ta={"center"}>Scheduled Time</Table.Th>
+              <Table.Th ta={"center"}>Actions</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>
             {interviews?.others?.map((interview: Interview) => (
-              <Table.Tr key={interview.id}>
+              <Table.Tr key={interview.id} ta={"center"}>
                 <Table.Td>{interview.student.user.fullName}</Table.Td>
                 <Table.Td>{interview.student.user.email}</Table.Td>
-                <Table.Td>{interview.status}</Table.Td>
+                <Table.Td>
+                  <Badge
+                    variant="light"
+                    radius={"xl"}
+                    color={
+                      interview.status === "Scheduled"
+                        ? "yellow"
+                        : interview.status === "Passed"
+                          ? "green"
+                          : "blue"
+                    }
+                  >
+                    {interview.status}
+                  </Badge>
+                </Table.Td>
                 <Table.Td>
                   {new Date(interview.date).toLocaleDateString("en-US")}
                 </Table.Td>
@@ -41,8 +118,55 @@ const Dashboard = () => {
                   {new Date(interview.time).toLocaleTimeString()}
                 </Table.Td>
                 <Table.Td>
-                  <Group align="center" gap={3}>
-                    <Text c={"blue"}>Change Status</Text>
+                  <Group justify="center">
+                    <Menu
+                      withArrow
+                      width={300}
+                      position="bottom"
+                      transitionProps={{ transition: "pop" }}
+                      withinPortal
+                    >
+                      <Menu.Target>
+                        <ActionIcon variant="subtle">
+                          <IconDots size={18} />
+                        </ActionIcon>
+                      </Menu.Target>
+                      <Menu.Dropdown>
+                        <Menu.Label>Actions</Menu.Label>
+                        <Menu.Item onClick={() => handleEditClick(interview)}>
+                          Edit
+                        </Menu.Item>
+                        <Menu.Divider />
+                        <Menu.Label>Status Actions</Menu.Label>
+                        <Menu.Item
+                          onClick={() =>
+                            handleStatusChange(interview.id, {
+                              status: "Passed"
+                            })
+                          }
+                        >
+                          Pass
+                        </Menu.Item>
+                        <Menu.Item
+                          onClick={() =>
+                            handleStatusChange(interview.id, {
+                              status: "Rejected"
+                            })
+                          }
+                        >
+                          Reject
+                        </Menu.Item>
+                        <Menu.Item
+                          onClick={() =>
+                            handleStatusChange(interview.id, {
+                              status: "Postponed"
+                            })
+                          }
+                        >
+                          Postpone
+                        </Menu.Item>
+                      </Menu.Dropdown>
+                    </Menu>
                   </Group>
                 </Table.Td>
               </Table.Tr>
@@ -50,6 +174,28 @@ const Dashboard = () => {
           </Table.Tbody>
         </Table>
       </Card>
+      <Modal opened={opened} onClose={close} title="Edit Interview Time">
+        <Stack>
+          <DateTimePicker
+            defaultValue={selectedDate}
+            label="Pick Date and Time"
+            placeholder="Schedule Interview"
+            onChange={(date) => {
+              console.log(date);
+
+              setSelectedDate(date);
+            }}
+            w={"100%"}
+          />
+          <Button
+            onClick={handleUpdateClick}
+            loading={schedulingInterview}
+            fullWidth
+          >
+            Update Interview
+          </Button>
+        </Stack>
+      </Modal>
     </Stack>
   );
 };
