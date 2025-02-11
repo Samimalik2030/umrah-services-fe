091d2074b692738584e9f6db0d288cf63342b852
farhanjamil259 <farhanjamil259@gmail.com>
2025-02-11 13:05:03 +0500

    chore: added env files

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,7 @@ import { Api } from "./Api";
 const http = new Api();
 
 // http.instance.defaults.baseURL = "https://company-api.mpacoaching.com";
-http.instance.defaults.baseURL = "https://api.mpacoaching.com";
+http.instance.defaults.baseURL = import.meta.env.VITE_API_URL;
 // http.instance.defaults.baseURL = "https://tn-nest.onrender.com";
 // http.instance.defaults.baseURL = "http://localhost:3000";
 
